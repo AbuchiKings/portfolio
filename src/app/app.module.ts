de7c4f4ded8eced7e06871062fb06a226808fd06
Abuchi Kingsley <abuchikingsley76@gmail.com>
2019-09-30 21:07:15 +0100

    Added legin and contact components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,24 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './main/home.component';
+import { LoginComponent } from './main/login.component';
+import { ContactComponent } from './main/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       {path: 'home', component: HomeComponent},
+      {path: 'login', component: LoginComponent},
+      {path: 'contact', component: ContactComponent},
       {path: '', redirectTo: 'home', pathMatch:'full'},
       {path: '**', component: HomeComponent}
     ])
