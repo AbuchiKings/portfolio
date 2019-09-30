a0aeb3f16c37c4b98c2c9c7fc234a6b8597016e8
Abuchi Kingsley <abuchikingsley76@gmail.com>
2019-09-30 12:47:29 +0100

    Added footer and nav components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <nav-bar></nav-bar>
+  <router-outlet></router-outlet>
+  <ft></ft>
+  `,
 })
 export class AppComponent {
   title = 'portfolio';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './main/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', component: HomeComponent},
+      {path: '', redirectTo: 'home', pathMatch:'full'},
+      {path: '**', component: HomeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
